f800360ec2d9a780f99a458e2e1bfb3d1bd928fc
RockyAo <542622608@qq.com>
2016-06-07 00:26:09 +0800

    no message

--- Podfile
+++ Podfile
@@ -4,7 +4,7 @@
 use_frameworks!
 
 target 'RACustomCameraSwift' do
-pod 'Aspects', '~> 1.4.1' 
+ 
 end
 
 target 'RACustomCameraSwiftTests' do
--- Pods/Target Support Files/Pods-RACustomCameraSwift/Pods-RACustomCameraSwift-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RACustomCameraSwift : NSObject
-@end
-@implementation PodsDummy_Pods_RACustomCameraSwift
-@end
